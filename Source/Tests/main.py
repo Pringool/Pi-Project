f4ef86a9cc5f538fb63c4c0558d3ef30dd129c13
Pringool <57721643+Pringool@users.noreply.github.com>
2022-02-25 11:44:55 +0100

    ^

---
+++ Source/Tests/main.py
@@ -0,0 +1,16 @@
+from asyncio.windows_events import NULL
+import random
+
+
+class color:
+    def __init__(self, possible_colors[]):
+        self.possibe_colors = possible_colors
+        
+    def get_random_color(self):
+        self.chosen_color = random.choice(self.possibe_colors)
+        return self.chosen_color
+    
+    
+class button_handler():
+    def __init__(self):
+        
