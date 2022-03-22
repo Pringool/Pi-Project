ee57edd9e231f6e6199347e429090e844709110a
Davidedercoole <davide.ulbrich@students.tbs1.de>
2022-03-22 20:20:47 +0100

    LEDs Hinzugefügt

--- Source/Tests/Button_handler.py
+++ Source/Tests/Button_handler.py
@@ -1,27 +1,51 @@
-from gpiozero import Button as button
+from gpiozero import Button as button, LED
 import time 
 
-button1 = button (24)
-button2 = button (25)
-button3 = button (23)
+
+
+class Settings():
+    button1 = button (23)
+    button2 = button (24)
+    button3 = button (25)
+    button4 = button (17)
+    button5 = button (27)
+
+    led1 = LED(22)
+    led2 = LED(10)
+    led3 = LED(9)
+    led4 = LED(11)
+    led5 = LED(5)
+
+    def leuchten1():
+        Settings.led1.on()
+
+    def leuchten2():
+        Settings.led2.on()
+
+    def leuchten3():
+        Settings.led3.on()
+
+    def leuchten4():
+        Settings.led4.on()
+
+    def leuchten5():
+        Settings.led5.on()
+
+    
+    
+
+
+Settings.button1.when_pressed = Settings.leuchten1
+Settings.button2.when_pressed = Settings.leuchten2
+Settings.button3.when_pressed = Settings.leuchten3
+Settings.button4.when_pressed = Settings.leuchten4
+Settings.button5.when_pressed = Settings.leuchten5
 
 
 
-def test1():
-    print("Button 1")
-
-def test2():
-    print("Button 2")
-
-def test3():
-    print("Button 3")
 
 
-button1.when_pressed = test1
 
-button2.when_pressed = test2
-
-button3.when_pressed = test3
 
 try: 
   while True:
