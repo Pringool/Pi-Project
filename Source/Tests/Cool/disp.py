09e2fa0a5f77f932d45d1bc2b81fb6a0ce2d90fd
Pringool <57721643+Pringool@users.noreply.github.com>
2022-03-23 19:27:38 +0100

    more changes

--- Source/Tests/Cool/disp.py
+++ Source/Tests/Cool/disp.py
@@ -10,7 +10,6 @@ from lib_oled96 import ssd1306
 
 class Display:
   def __init__(self):
-    self.c = Colors()
     self.max_screen_x = 120
     self.max_screen_y = 50
     self.i2cbus = SMBus(1)    
@@ -22,9 +21,14 @@ class Display:
     self.oled.display()
 
 
-  def display_color(self):
-    self.c.choose_color()
-    self.disp.text(((self.max_screen_x // 2) - 20, self.max_screen_y // 2), f"{self.c.get_color()}", fill=1)
+  def display_color(self, color):
+    self.disp.text(((self.max_screen_x // 2) - 20, self.max_screen_y // 2), f"{color}", fill=1)
+    self.oled.display()
+    
+  def display_start(self):
+    self.disp.text(((self.max_screen_x // 2) - 20, self.max_screen_y // 2), f"Press", fill=1)
+    self.disp.text(((self.max_screen_x // 2) - 20, self.max_screen_y  // 2 + 10), "any Button",fill=1)
+    self.disp.text(((self.max_screen_x // 2) - 20, self.max_screen_y // 2 + 20), "to continue", fill=1)
     self.oled.display()
   
   
--- Source/Tests/Cool/main.py
+++ Source/Tests/Cool/main.py
@@ -1,25 +1,80 @@
 from colors import Colors
 from disp import Display
-from rounds import Round_system
-#from Button_handler import buttons
+from gpiozero import LED, Button as button
 import time
+  
+  
+button1 = button(23)
+button2 = button(24)
+button3 = button(25)
+button4 = button(17)
+button5 = button(27)
+
+red = LED(22)
+blue = LED(10)
+green = LED(9)
+white = LED(11)
+yellow = LED(5)
+
   
 class Game:
   def __init__(self):
-    self.disp = display()
+    self.disp = Display()
+    self.c = Colors()
+    self.level = 0
+    self.state = "Start"
   def display_c(self):
-    self.disp.display_color()
+    self.disp.display_color(self.c.get_color())
   def clean_display(self):
     self.disp.clear()
     
     
   def start(self):
     self.clean_display()
-    r.round()
+    self.round_system()
+    self.disp.display_start()
+    
+  def round_system(self):
+    self.clean_display()
+    if self.state == "Play":
+      self.c.choose_color()
+      self.display_c()
+  def button1(self):
+    if self.state == "Start":
+      self.state = "Play"
+      
+  def button2(self):
+    if self.state == "Start":
+      self.state = "Play"
+      
+  def button3(self):
+    if self.state == "Start":
+      self.state = "Play"
+      
+  def button4(self):
+    if self.state == "Start":
+      self.state = "Play"
+      
+  def button5(self):
+    if self.state == "Start":
+      self.state = "Play"
+      
+      
+button1.when_pressed = game.button1
+
+button2.when_pressed = game.button2
+
+button3.when_pressed = game.button3
+
+button4.when_pressed = game.button4
+
+button5.when_pressed = game.button5
+      
+      
   
 game = Game()
 game.start()
-r = Round_system()
+
 try:
   while True:
     time.sleep(0.5)
