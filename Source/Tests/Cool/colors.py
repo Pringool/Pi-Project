4bdff5e8114c2bf8a4f8287abcc6c494fa170265
Pringool <57721643+Pringool@users.noreply.github.com>
2022-03-24 14:50:30 +0100

    Help me

--- Source/Tests/Cool/colors.py
+++ Source/Tests/Cool/colors.py
@@ -3,10 +3,10 @@ import random
 
 class Colors:
   def __init__(self):
-    self._color = ["Grün","Blau","Weiß","Rot","Gelb"]
+    self._color = ["green","blue","white"]#,"red","yellow"]
     
   def get_color(self):
-    return self.color
+    return self.color.upper()
     
   def choose_color(self):
     self.color = random.choice(self._color)
--- Source/Tests/Cool/main.py
+++ Source/Tests/Cool/main.py
@@ -4,29 +4,39 @@ from gpiozero import LED, Button as button
 import time
   
   
-button1 = button(23)
-button2 = button(24)
-button3 = button(25)
-button4 = button(17)
-button5 = button(27)
-
-red = LED(22)
-blue = LED(10)
-green = LED(9)
-white = LED(11)
-yellow = LED(5)
-
   
 class Game:
+  button1 = button(23)
+  button2 = button(24)
+  button3 = button(25)
+  button4 = button(17)
+  button5 = button(27)
+
+  red = LED(22)
+  blue = LED(10)
+  green = LED(9)
+  white = LED(11)
+  yellow = LED(5)
+  
   def __init__(self):
     self.disp = Display()
     self.c = Colors()
     self.level = 0
     self.state = "Start"
+
+    
   def display_c(self):
     self.disp.display_color(self.c.get_color())
+    
   def clean_display(self):
     self.disp.clear()
+    
+  def clean_led(self):
+    self.red.off()
+    self.blue.off()
+    self.green.off()
+    self.white.off()
+    self.yellow.off()
     
     
   def start(self):
@@ -34,46 +44,108 @@ class Game:
     self.round_system()
     self.disp.display_start()
     
+  def check_right(self, button_pressed):
+    if button_pressed == "1":
+      if self.c.get_color() == "WHITE":
+        print("correct!")
+        self.round_system()
+      else:
+        print("Wrong!")
+        self.clean_led()
+        self.round_system()
+        
+    elif button_pressed == "2":
+      if self.c.get_color() == "GREEN":
+        print("correct!")
+        self.round_system()
+      else:
+        print("Wrong!")
+        self.clean_led()
+        self.round_system()
+        
+    elif button_pressed == "3":
+      if self.c.get_color() == "BLUE":
+        print("correct!")
+        self.round_system()
+      else:
+        print("Wrong!")
+        self.clean_led()
+        self.round_system()
+        
+    elif button_pressed == "4":
+      if self.c.get_color() == "YELLOW":
+        print("correct!")
+        self.round_system()
+      else:
+        print("Wrong!")
+        self.clean_led()
+        self.round_system()
+        
+    elif button_pressed == "5":
+      if self.c.get_color() == "RED":
+        print("correct!")
+        self.round_system()
+      else:
+        print("Wrong!")
+        self.clean_led()
+        self.round_system()
+        
   def round_system(self):
     self.clean_display()
     if self.state == "Play":
       self.c.choose_color()
       self.display_c()
-  def button1(self):
+      
+  def button1_p(self):
     if self.state == "Start":
       self.state = "Play"
+      self.round_system()
+    else: 
+      Game.white.on()
+      self.check_right("1")
       
-  def button2(self):
+  def button2_p(self):
     if self.state == "Start":
       self.state = "Play"
+      self.round_system()
+    else: 
+      Game.green.on()
+      self.check_right("2")
       
-  def button3(self):
+  def button3_p(self):
     if self.state == "Start":
       self.state = "Play"
+      self.round_system()
+    else: 
+      Game.blue.on()
+      self.check_right("3")
       
-  def button4(self):
+  def button4_p(self):
     if self.state == "Start":
       self.state = "Play"
+      self.round_system()
+    else: pass
       
-  def button5(self):
+  def button5_p(self):
     if self.state == "Start":
       self.state = "Play"
+      self.round_system()
+    else: pass
       
-      
-button1.when_pressed = game.button1
-
-button2.when_pressed = game.button2
-
-button3.when_pressed = game.button3
-
-button4.when_pressed = game.button4
-
-button5.when_pressed = game.button5
-      
-      
-  
 game = Game()
-game.start()
+game.start()      
+
+game.button1.when_pressed = game.button1_p
+
+game.button2.when_pressed = game.button2_p
+
+game.button3.when_pressed = game.button3_p
+
+game.button4.when_pressed = game.button4_p
+
+game.button5.when_pressed = game.button5_p
+
+
 
 try:
   while True:
@@ -81,4 +153,5 @@ try:
     
 except KeyboardInterrupt:
   game.clean_display()
-  print("Bye Bye")
+  game.clean_led()
+  print("\nBye Bye\n")
